84605a196a5f5bd45c30b1fcc11c75d2715c764c
karezi <karezi1992@gmail.com>
2014-12-16 11:11:48 +0800

    Add all files from old project

---
+++ js/function.js
@@ -0,0 +1,60 @@
+﻿$(document).ready(function () {
+    music = document.getElementById("m");
+    music.play();
+    $("#music").change(function () {
+        music.pause();
+        $("#m").remove();
+        $("#button").after("<audio loop id='m'><source id='source' src='/source/" + $("#music").val() + "' type='audio/mpeg'></audio>");
+        $("#button").attr("src", "/source/pause.png");
+        music = document.getElementById("m");
+        music.play();
+    });
+    $("#button").click(function () {
+        if ($("#button").attr("src") == "/source/pause.png") {
+            $("#button").attr("src", "/source/play.png");
+            music.pause();
+        }
+        else if ($("#button").attr("src") == "/source/play.png") {
+            $("#button").attr("src", "/source/pause.png");
+            music.play();
+        }
+    });
+
+    picid = 0;
+    tmp = 0;
+    $("#backPic").click(function () {
+        do {
+            tmp = parseInt(Math.random()*(5-1+1)+1);
+        }while(tmp == picid);
+        picid = tmp;
+        $("body").css("background-image", "url('/source/b" + picid + ".jpg')");
+    });
+
+    $("#color").change(function () {
+        $("body").css("background-image", "none");
+        picid = 0;
+        $("body").css("background-color", "transparent");
+        zeros = "";
+        var colorStr = parseInt($("#color").val(), 10).toString(16);
+        var zero = 6 - colorStr.length;
+        for(var i = 0; i < zero; i++){
+            zeros += "0"; 
+        }
+        $("body").css("background-color", "#" + zeros + colorStr);
+    });
+});
+/*$(document).ready(function () {
+    music = document.getElementById("m");
+    music.play();
+    $("#button").click(function () {
+        if ($("#button").attr("src") == "/source/pause.png") {
+            $("#button").attr("src", "/source/play.png");
+            music.pause();
+        }
+        else if ($("#button").attr("src") == "/source/play.png") {
+            $("#button").attr("src", "/source/pause.png");
+            music.play();
+        }
+    });
+
+});*/
